7f6c1fa00f0fe1be944cc15dfa7837385f1c8ce3
Arnold Moya <dev.arn.ml@gmail.com>
2025-01-10 18:54:03 -0300

    Refactor Home component layout for improved readability

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,16 @@ import CatDescription from "@/components/CatDescription/CatDescription";
 
 export default function Home() {
   return (
-    <>
-      <div className="max-w-screen-sm p-2 pt-28 mx-auto space-y-4">        
-        <h1 className="text-3xl font-semibold text-gray-900 text-center py-2">
-          Entiende papers científicos en 
-          <span className="relative m-2">
-            <span className="block absolute -inset-1 -skew-y-3 bg-indigo-600" aria-hidden="true"></span>
-            <span className="relative text-white">minutos</span>
-          </span>
-        </h1>
-        <CatDescription/> 
-        <Player/>               
-v      </div>
-    </>
+    <div className="max-w-screen-sm p-2 pt-28 mx-auto space-y-4">        
+      <h1 className="text-3xl font-semibold text-gray-900 text-center py-2">
+        Entiende papers científicos en 
+        <span className="relative m-2">
+          <span className="block absolute -inset-1 -skew-y-3 bg-indigo-600" aria-hidden="true"></span>
+          <span className="relative text-white">minutos</span>
+        </span>
+      </h1>
+      <CatDescription/> 
+      <Player/>               
+    </div>
   );
 }
